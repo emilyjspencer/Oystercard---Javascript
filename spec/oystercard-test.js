4d31629a1a62584e8eb3bb8eee5f5ab9a0942c44
emilyjspencer <emilyspencer89@hotmail.co.uk>
2020-05-12 20:47:59 +0100

    Refactors to use beforeEach function to DRY up code

--- spec/oystercard-test.js
+++ spec/oystercard-test.js
@@ -3,13 +3,15 @@
 describe('OysterCard', function() {
   var oystercard;
 
+  beforeEach(function() {
+    oystercard = new Oystercard();
+  });
+
   it('has a balance of 0 by default', function() {
-    oystercard = new OysterCard;
     expect(oystercard.balance).toEqual(0)
   });
 
   it('can have credit added to it', function() {
-      oystercard = new OysterCard;
       oystercard.topup(10)
       expect(oystercard.balance).toEqual(10)
   });
